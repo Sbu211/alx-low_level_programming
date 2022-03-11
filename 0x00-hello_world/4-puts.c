edb22375a3a35b09fc809c97b360ca0a00ba15d1
Sibusiso <sbudalenton@gmail.com>
2022-03-11 03:11:18 -0800

    added 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,10 @@
  * Returns 0
  */
 
-int main (void)
+int main(void)
 {
 	/* print a string to stdout*/
-	putchar ('"');
-	puts ("Programming is like building a multilingual puzzle");
+	putchar('"');
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
