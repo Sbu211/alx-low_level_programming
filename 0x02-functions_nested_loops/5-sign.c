4795dc2649bf4cabb0801640b9ee61ccc34a9434
Sibusiso <sbudalenton@gmail.com>
2022-03-16 11:06:36 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+
+/**
+ * print_sign -  name of the function
+ * @n: is the the number
+ * Description: Return 1 and + if positive,0 if zero and -1 and - if negative
+ * Return: 1 if positive and 0
+ */
+
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+
+		value = 1;
+		_putchar('1');
+	}
+
+	else if
+	{
+		value = 0;
+		_putchar('0');
+	}
+
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
+
+
+
+
+
+
+
+}
