3648383cc5de79f4aed6b086c5bab4994b3b6140
Sibusiso <sbudalenton@gmail.com>
2022-03-16 01:57:39 -0700

    added print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+
+
+
+/**
+ * print_alphabet - name of prototype function
+ *
+ * Desciption: It prints the alphabet in lower case
+ *
+ * Return: Nothing
+ *
+ */
+
+
+void print_alphabet(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz");
+
+}
