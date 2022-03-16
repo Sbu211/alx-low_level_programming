8a7f9391263b574ffff6a7ced0e610b8c3a79fd7
Sibusiso <sbudalenton@gmail.com>
2022-03-16 13:56:11 -0700

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - check main
+ * @a: integer a
+ * @b: integer b
+ * Description: Adds two intergers together
+ * Results: sum
+ */
+
+int add(int a, int b)
+{
+	int a, b;
+
+	sum = a + b;
+
+
+	return (sum);
+}
