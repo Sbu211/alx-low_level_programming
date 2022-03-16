0b016e9b831c2cfbdb46363180188aa2594e88d1
Sibusiso <sbudalenton@gmail.com>
2022-03-16 11:50:38 -0700

    add abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - check main
+ * @i: input in inter formm
+ * Description: get the absolute value
+ * Return: |int| is returned
+ */
+
+
+int _abs(int i)
+{
+	if (i >= 0)
+
+		return (i);
+
+	else
+
+		return (i * -1);
+}
