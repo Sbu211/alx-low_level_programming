56ad60791412e4a797006cc0ff8f6f3fcf3a9493
Sibusiso <sbudalenton@gmail.com>
2022-03-16 02:38:45 -0700

    added print 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib>
+#include "main.h"
+
+
+/**
+ * void print_alphabet_x10(void)
+ *
+ *Description:The fucntion prints the alphabet 10 times
+ */
+
+
+void print_alphabet_10x(void)
+
+{	int i;
+
+	while (i < 10)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+		i++;
+	}
+
+}
