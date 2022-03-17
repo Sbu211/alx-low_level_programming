4b6dc41aacb988dcc44796305f2ee624f78c9cb4
Sibusiso <sbudalenton@gmail.com>
2022-03-17 09:58:19 -0700

    fixed code largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,15 +11,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >=b && a >== c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b >= a && b >=c)
+	else if (b >= a && b >= c)
 	{
 		largetst = b;
 	}
-	else if (c >= a && c >=b)
+	else if (c >= a && c >= b)
 	{
 		largest = c;
 	}
